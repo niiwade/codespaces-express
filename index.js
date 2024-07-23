a5a4cac26c6ae667fec75933e47f81dec8728214
GlkwseEugene <165899947+GlkwseEugene@users.noreply.github.com>
2024-07-23 16:56:17 +0000

    Eugene Ohene End of term Exam

--- index.js
+++ index.js
@@ -2,9 +2,53 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/student', (req, res) => {
+  res.send({
+    id:1,
+    name:'Eugene',
+    class:'web dev',
+    lastname:'Ohene'
+  })
+  });
+
+app.get('/student/:id', (req, res) => {
+  res.send({
+    id:1,
+    name:'Eugene',
+    class:'web dev',
+    lastname:'Ohene'
+  })
+  });
+
+
+  app.post('/student/post',(req, res) => {
+  res.send({
+    id:2,
+    name:'Gabriel',
+    class:'web dev',
+    lastname:'Acolaste'
+  })
+  });
+
+
+  app.put('/student/:id',(req, res) => {
+  res.send({
+    id:1,
+    name:'Osman',
+    class:'design manufacturing',
+    lastname:'Umar'
+  })
+  });
+
+
+  app.delete('/student/:id',(req, res) => {
+  res.send({
+    id:1,
+    name:'Jephthah',
+    class:'web dev',
+    lastname:'Essel'
+  })
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
