7d1b7e228bf154c686241db5ad063e665efd9930
ISRAEL AZIDOR <157823196+israelqwedwo@users.noreply.github.com>
2024-07-23 21:13:25 +0000

    ISRAEL AZIDOR This is my final work. I had issues doing the PR because my PR was merged with another person's work. So i was forced to redo everything again after troubleshooting it severally to find solutions

--- index.js
+++ index.js
@@ -2,9 +2,56 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/students', (req, res) => {
+  res.send({
+    Name: 'Israel Azidor',
+    Course: 'Web Development',
+    Level: '100',
+    createDAt:'23-07-2024'
+  })
 })
+
+
+app.get('/students/:id', (req, res) => {
+  res.send({
+    ID:'1',
+    Name: 'Israel Azidor',
+    Course: 'Web Development',
+    Level: '100',
+    createDAt:'23-07-2024'
+  })
+})
+
+app.post('/student/post', (req, res) => {
+  res.send({
+    ID:'2',
+    Name: 'Israel LarryKing',
+    Course: 'Web Development',
+    Level: '100',
+    createDAt:'23-07-2024'
+  })
+})
+
+app.put('/student/:id', (req, res) => {
+  res.send({
+    ID:'3',
+    Name: 'Anita Yen',
+    Course: 'Web Development',
+    Level: '100',
+    createDAt:'23-07-2024'
+  })
+})
+
+app.delete('/student/:id', (req, res) => {
+  res.send({
+    ID:'1',
+    Name: 'Israel Azidor',
+    Course: 'Web Development',
+    Level: '100',
+    createDAt:'23-07-2024'
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
