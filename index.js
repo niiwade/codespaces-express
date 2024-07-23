b6e1334311046e0e24e4a6ed990becd2c4586218
billssilver <165900129+billssilver@users.noreply.github.com>
2024-07-23 16:34:37 +0000

    Gabriel Acolatse End of term Exams

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
+app.get('/student/:id',(req, res) => {
+  res.send({
+  id:1, 
+  name: 'kofi',
+  class:'web dev',
+  lastname:'Gabriel'
+ 
+  })
+  });
+app.post('/student/post',(req,res) =>{
+ res.send({
+ id:2,
+ name:'emmanuel',
+ class:'web dev',
+ lastname:'mends'
+})
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.put ( '/student/:id',(req,res) =>{
+res.send
+({
+id:1,
+name:'joel',
+class:'web dev',
+lastname:'mensah'
+})
 })
 
+app.delete('/student/:id',(req,res) => {
+  res.send({
+    id:1,
+    name:'Gabreil',
+    class:'web dev',
+    lastname:'Acolatse'
+
+  })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
