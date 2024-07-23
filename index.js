74e55b640c381bb3a06ded5cb5d5986a3799750b
Jephthah Essel <157823716+jevisexcell024@users.noreply.github.com>
2024-07-23 16:21:21 +0000

    Jephthah Essel This is an exams

--- index.js
+++ index.js
@@ -2,10 +2,57 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/student', (req, res) => {
+  res.send({
+    name:'Jephthah',
+    course:'web dev',
+    level:400
+  }
+)
+
+});
+
+app.get('/student/:id',(req, res) => {
+res.send({
+  id:1,
+  name:'Jephthah',
+  course :'web dev',
+  level:400
 })
+});
+
+
+app.post('/student/post',(req, res) => {
+  res.send({
+    id:200,
+    name:'Araba',
+    course :'web dev',
+  })
+  });
+
+  app.put('/student/:id',(req, res) => {
+    res.send({
+      id:1,
+      name:'Jevis',
+      course :'web dev',
+      level:400
+
+    })
+    });
+
+    app.delete('/student/:id',(req, res) => {
+      res.send({
+        id:1,
+        name:'Jephthah',
+        course :'web dev',
+        level:400
+
+      })
+      });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(` Jephthah Essel ${port}`)
 })
+
+
+
