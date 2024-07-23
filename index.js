c3f566cc7daf6251da1df8b8803eb5ba67436b32
Larry000 <159909962+Larry000@users.noreply.github.com>
2024-07-23 16:45:26 +0000

    Main: Badu Israel Larryking End of team exams. We were asked to create a database for students

--- index.js
+++ index.js
@@ -2,10 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/student/', (req, res) => {
+  res.send({
+ name: 'badu',
+ course: 'web dev',
+ level: 3,
 })
 
+});
+
+app.get('/student/:id', (req, res) => {
+  res.send({
+ id:1,   
+ name: 'badu',
+ course: 'web dev',
+ level: 3,
+})
+
+
+});
+
+app.post('/student/:post', (req, res) => {
+  res.send({
+ id: 2,   
+ name: 'badu',
+ course: 'web dev',
+ level: 3,
+})
+
+
+});
+
+app.put('/student/:id', (req, res) => {
+  res.send({
+ id: 1,   
+ name: 'badu',
+ course: 'web dev',
+ level: 3,
+})
+
+
+});
+
+app.delete('/student/:id', (req, res) => {
+  res.send({
+ id: 1,   
+ name: 'badu',
+ course: 'web dev',
+ level: 3,
+})
+
+
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ readme.md
@@ -0,0 +1,38 @@
+GET / api/student - Retrieve all student name
+{
+  "id": 12,
+  "name": "badu",
+  "course": "web dev",
+  "level": 3
+}
+
+GET / api/student/:id - Retrieve all specific student
+{
+  "name": "badu",
+  "course": "web dev",
+  "level": 3
+}
+
+POST / api/student - Create a new student
+{
+  "id": 2,
+  "name": "badu",
+  "course": "web dev",
+  "level": 3
+}
+
+PUT / api/student/:id - update an existing student
+{
+  "id": 1,
+  "name": "badu",
+  "course": "web dev",
+  "level": 3
+}
+
+DELETE / api/student/:id - Delete a student
+{
+  "id": 1,
+  "name": "badu",
+  "course": "web dev",
+  "level": 3
+}
